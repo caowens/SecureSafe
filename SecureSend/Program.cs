de9d945afec980323524cf0a0633bdb482ea08fa
Charles Owens <caowens256@gmail.com>
2023-05-06 15:58:39 -0500

    Encryption functionality added

--- SecureSend/Program.cs
+++ SecureSend/Program.cs
@@ -2,25 +2,66 @@
 using System.IO;
 using System.Security.Cryptography;
 
-namespace SecureTool
+class EncryptionTool
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length < 3)
         {
-            if (args.Length != 2)
+            Console.WriteLine("Usage: encryptiontool [encrypt/decrypt] [input file path] [output file path]");
+            return;
+        }
+
+        string command = args[0];
+        string inputFile = args[1];
+        string outputFile = args[2];
+
+        byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+        byte[] iv = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+
+        using (var aes = Aes.Create())
+        {
+            // aes.Key = key;
+            // aes.IV = iv;
+
+            aes.KeySize = 256;
+            aes.GenerateKey();
+            aes.GenerateIV();
+
+            if (command == "encrypt")
             {
-                Console.WriteLine("You must provide the name of a file to read and the name of a file to write.");
+                Encrypt(inputFile, outputFile, aes);
+            }
+            else if (command == "decrypt")
+            {
+                Decrypt(inputFile, outputFile, aes);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command.");
                 return;
             }
+        }
+    }
 
-            var sourceFilename = args[0];
-            var destinationFilename = args[1];
-
-            using (var sourceStream = File.OpenRead(sourceFilename))
-            using (var destinationStream = File.Create(destinationFilename))
+    static void Encrypt(string inputFile, string outputFile, SymmetricAlgorithm algorithm)
+    {
+        using (var inputStream = new FileStream(inputFile, FileMode.Open))
+        {
+            using (var outputStream = new CryptoStream(new FileStream(outputFile, FileMode.Create), algorithm.CreateEncryptor(), CryptoStreamMode.Write))
             {
-                sourceStream.CopyTo(destinationStream);
+                inputStream.CopyTo(outputStream);
+            }
+        }
+    }
+
+    static void Decrypt(string inputFile, string outputFile, SymmetricAlgorithm algorithm)
+    {
+        using (var inputStream = new CryptoStream(new FileStream(inputFile, FileMode.Open), algorithm.CreateDecryptor(), CryptoStreamMode.Read))
+        {
+            using (var outputStream = new FileStream(outputFile, FileMode.Create))
+            {
+                inputStream.CopyTo(outputStream);
             }
         }
     }
